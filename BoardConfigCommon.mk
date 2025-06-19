8ec7c9d9fca24de238a308033082976b9f613eed
TechPanelGM <giomook2003@gmail.com>
2025-06-19 18:39:23 +0000

    sm7250-common: migrate to soong_config_set,camera,override_format_from_reserved vendor/lineage/config/BoardConfigSoong.mk:39: warning: TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED is deprecated, please migrate to soong_config_set,camera,override_format_from_reserved

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,7 +45,7 @@ TARGET_BOOTLOADER_BOARD_NAME := lito
 TARGET_NO_BOOTLOADER := true
 
 # Camera
-TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
+$(call soong_config_set,camera,override_format_from_reserved,true)
 TARGET_USES_YCRCB_CAMERA_ENCODE := true
 
 # Display
